23e1ea191a8a3e9e09ea1e49bacac46992146b69
Prajapati Kaushik <prajapatikaushik6292@gmail.com>
2025-02-22 16:07:40 +0530

    name issue

--- src/Pages/Product/Product.jsx
+++ src/Pages/Product/Product.jsx
@@ -3,7 +3,7 @@ import { ShopContext } from "../../Context/ShopContext";
 import { useParams } from "react-router-dom";
 import Breadcrum from "../../components/Breadcrum/Breadcrum";
 import ProductDisplay from "../../components/ProductDisplay/ProductDisplay";
-import DescriptionBox from "../../components/descriptionBox/descriptionBox";
+import DescriptionBox from "../../components/DescriptionBox/DescriptionBox";
 import RelatedProducts from "../../components/RelatedProducts/RelatedProducts";
 
 const Product = () => {
